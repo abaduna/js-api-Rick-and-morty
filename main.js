39a2b1c309df9c7b01cce9592ccd8e3b7f5c8bbf
abaduna <64230830+abaduna@users.noreply.github.com>
2023-10-11 16:58:51 -0300

    funcionalidad locacion

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const apiRick=async(pagina)=>{
     let url ="https://rickandmortyapi.com/api/character/?page="+pagina
     const api = await fetch(url)
     const  data = await api.json()
-    console.log(data)
+    
     divRes = document.querySelector("#resultado")
     divRes.innerHTML = ""
     data.results.map(item=>{
@@ -22,5 +22,27 @@ const apiRick=async(pagina)=>{
     })
 
 }
+apilocation =async(pagina2)=>{
+    let url = "https://rickandmortyapi.com/api/location?page="+pagina2
+    const api = await fetch(url)
+    const data2 = await api.json()
+    console.log(data2)
+    divRes2 = document.getElementById("resultado2")
+    divRes2.innerHTML = ""
+    data2.results.map(item=>{
+        divItem2 =document.createElement("div")
+        divItem2.innerHTML=`<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Es de la dimension ${item.dimension}  </h6>
+          <p class="card-text">Es del tipo ${item.type}</p>
+          
+        </div>
+      </div>`
+        
+        divRes2.appendChild(divItem2)
+    })
+}
+apilocation(1)
 apiRick(1)
 //<p class="card-text"> ${item.gender == "Male" ?"femenino"}</p>
